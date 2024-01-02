31f7bc4278cbac0fd5c1f3364dd9f71f0f9e56ee
aricma <adrian@aricma.com>
2024-01-02 21:06:58 +0100

    change: rewrite the error page and reduce the error html component to only the error content

--- web_interface/private/pages/make_error_page.py
+++ web_interface/private/pages/make_error_page.py
@@ -1,5 +1,8 @@
+from web_interface.private.components.make_footer_center_html import make_footer_center_html
+from web_interface.private.components.make_header_main_footer_html import make_header_main_footer_html
 from web_interface.private.components.make_html_file import make_html_file
 from web_interface.private.features.make_error_html import make_error_html
+from web_interface.private.features.make_public_page_html import make_public_page_html
 
 
 def make_error_page(
@@ -8,10 +11,16 @@ def make_error_page(
 ) -> str:
     return make_html_file(
         title=f"Developer Dashboard | {error_code}",
-        body=make_error_html(
-            error_code=error_code,
-            message=message,
-            action_link="/dashboard",
-            action_title="Return Back To Dashboard",
-        ),
+        body=[
+            make_header_main_footer_html(
+                header=make_public_page_html(),
+                main=make_error_html(
+                    error_code=error_code,
+                    message=message,
+                    action_link="/dashboard",
+                    action_title="Return Back To Dashboard",
+                ),
+                footer=make_footer_center_html()
+            ),
+        ]
     )
