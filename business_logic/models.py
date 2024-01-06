97b9c5748908daa106272c4566db49de1f6e3805
aricma <adrian@aricma.com>
2024-01-06 19:38:12 +0100

    add: new domain model for a Task

--- business_logic/models.py
+++ business_logic/models.py
@@ -1,4 +1,24 @@
-from typing import Set, Union, Literal
-
+import dataclasses
+from typing import Set, Union, Literal, List
 
 Permissions = Set[Union[Literal["READ_ALL", "READ_MINE", "READ_AND_WRITE_ALL"]]]
+
+TaskID = str
+DeveloperID = str
+Date = str
+
+
+@dataclasses.dataclass
+class Task:
+    id: TaskID
+    title: str
+    description: str
+    date_created: Date
+    date_finished: Date
+    story_points: int
+    assignees: List[DeveloperID]
+    subtasks: List[TaskID]
+
+
+FinishedTask = Task
+TaskInProgress = Task
