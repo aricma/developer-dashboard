ea8d086f4fd48c74b99757f1d9e3ec2b94dddd15
aricma <adrian@aricma.com>
2024-01-07 19:06:06 +0100

    add: todo

--- web_interface/private/__js_files__/charts.js
+++ web_interface/private/__js_files__/charts.js
@@ -40,6 +40,7 @@
             // ...data['average_developer_velocity'],
             // we take the data that is available to get the max value for the y axis
             // if there is no data for developer_velocity we fallback and display average_developer_velocity
+            // TODO: we do not have a solution for when all are empty
             ...(data['developer_velocity'].length > 0 ? data['developer_velocity'] : data['average_developer_velocity']),
         ].map(each => each.y)
         const averageValue = allYData.reduce((sum, each) => sum + each, 0) / (allYData.length || 1)
