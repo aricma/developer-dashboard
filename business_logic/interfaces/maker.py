fafad9f70ece3ce45ad219443d00ee78a381c1d7
aricma <adrian@aricma.com>
2024-01-12 14:51:01 +0100

    add: Maker interface

---
+++ business_logic/interfaces/maker.py
@@ -0,0 +1,11 @@
+from abc import ABC, abstractmethod
+from typing import Generic, TypeVar
+
+T = TypeVar("T")
+
+
+class Maker(ABC, Generic[T]):
+
+    @abstractmethod
+    def make(self) -> T:
+        ...
