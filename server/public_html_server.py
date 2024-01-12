dc57827db46c4ab2b33c3c7fefe2c06461ded3fa
aricma <adrian@aricma.com>
2024-01-12 14:24:51 +0100

    change: use authentication business logic in public html server refactor: reformat code

--- server/public_html_server.py
+++ server/public_html_server.py
@@ -3,7 +3,7 @@ from typing import Optional
 from fastapi import FastAPI, Form
 from starlette.responses import HTMLResponse, FileResponse, RedirectResponse
 
-from business_logic._business_logic import BusinessLogic
+from business_logic.authentication_business_logic import AuthenticationBusinessLogic
 from server import constants
 from server.authentication_utils import create_authentication_token_cookie_value, \
     create_expired_authentication_token_cookie_value
@@ -14,24 +14,22 @@ public_app = FastAPI(
     title="Developer Dashboard Web Server(Public)",
 )
 
-business_logic = BusinessLogic(
+authentication_business_logic = AuthenticationBusinessLogic(
     path_to_accounts_yml_file=str(constants.PATH_TO_ACCOUNTS_YML_FILE),
-    path_to_developers_json_file=str(constants.PATH_TO_DEVELOPERS_JSON_FILE),
-    path_to_tasks_json_file=str(constants.PATH_TO_TASKS_JSON_FILE),
 )
 
 
 @public_app.get("/sign-in")
 async def get_login_page() -> HTMLResponse:
-    return HTMLResponse(
-        content=make_sign_in_page()
-    )
+    return HTMLResponse(content=make_sign_in_page())
 
 
 @public_app.post("/sign-in")
 async def login(email: str = Form(), password: str = Form()) -> RedirectResponse:
     return redirect_after_successful_sign_in(
-        authentication_token=business_logic.unsafe_login(email=email, password=password)
+        authentication_token=authentication_business_logic.unsafe_login(
+            email=email, password=password
+        )
     )
 
 
@@ -40,33 +38,33 @@ async def logout() -> RedirectResponse:
     return RedirectResponse(
         status_code=303,
         url="/sign-in",
-        headers={
-            "Set-Cookie": create_expired_authentication_token_cookie_value()
-        },
+        headers={"Set-Cookie": create_expired_authentication_token_cookie_value()},
     )
 
 
 @public_app.get("/register")
 async def get_register_page() -> HTMLResponse:
-    return HTMLResponse(
-        content=make_register_page()
-    )
+    return HTMLResponse(content=make_register_page())
 
 
 @public_app.post("/register")
 async def register(
-        name: str = Form(),
-        email: str = Form(),
-        password: str = Form(),
+    name: str = Form(),
+    email: str = Form(),
+    password: str = Form(),
 ) -> RedirectResponse:
-    business_logic.unsafe_register_account(name, email, password)
-    authentication_token = business_logic.unsafe_login(email=email, password=password)
+    authentication_business_logic.unsafe_register_account(name, email, password)
+    authentication_token = authentication_business_logic.unsafe_login(
+        email=email, password=password
+    )
     return redirect_after_successful_sign_in(authentication_token)
 
 
 # ⚠️ this will serve everything
 @public_app.get("/{file_path}")
-async def serve_all_files_that_requested_by_html_files(file_path: Optional[str] = None) -> FileResponse:
+async def serve_all_files_that_requested_by_html_files(
+    file_path: Optional[str] = None,
+) -> FileResponse:
     resolved_file_path = "index.html" if not file_path else file_path
     return FileResponse(constants.PATH_TO_HTML_FILES / resolved_file_path)
 
