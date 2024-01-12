7bc98a6b1d72530af4bbb0fc7c185aec834d1a08
aricma <adrian@aricma.com>
2024-01-12 20:00:23 +0100

    add: Date model to contain the date related logic and reduce the options for errors across the code base

---
+++ business_logic/models/date.py
@@ -0,0 +1,38 @@
+from dateutil import parser
+import datetime
+
+
+class Date:
+
+    def __init__(self, year: int, month: int, day: int):
+        self._year = year
+        self._month = month
+        self._day = day
+
+    def from_string(self, date_as_string: str) -> "Date":
+        parsed_date = parser.parse(date_as_string)
+        return self.from_datetime_date(datetime_date=parsed_date)
+
+    @staticmethod
+    def from_datetime_date(datetime_date: datetime.date) -> "Date":
+        return Date(
+            year=datetime_date.year,
+            month=datetime_date.month,
+            day=datetime_date.day,
+        )
+
+    def to_string(self) -> str:
+        return f"{self._year}-{self._month}-{self._day}"
+
+    def to_datetime_date(self) -> datetime.date:
+        return datetime.date(
+            year=self._year,
+            month=self._month,
+            day=self._day,
+        )
+
+    def add_days(self, days: int) -> "Date":
+        return ...
+
+    def is_weekend(self) -> bool:
+        return self.to_datetime_date().weekday() > 4  # 5 & 6 are Sat and Sun
