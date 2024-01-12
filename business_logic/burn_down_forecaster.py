9988ad7f8456698c5c5d2b397c82a6a9a3507efb
aricma <adrian@aricma.com>
2024-01-12 19:44:42 +0100

    remove: unused Task in burn_down_forecaster and refactor code

--- business_logic/burn_down_forecaster.py
+++ business_logic/burn_down_forecaster.py
@@ -1,26 +1,14 @@
-import dataclasses
 from datetime import timedelta as duration
-from typing import Dict
 from dateutil.parser import parse as to_date
 
 from business_logic.date_skipper import DateSkipper
 from business_logic.models.burn_down_forecast import BurnDownForecast
 from business_logic.models.burn_down_forecastable_task import BurnDownForecastableTask
 
-
-@dataclasses.dataclass
-class Task:
-    id: TaskID
-    start_date: Date
-    story_points: StoryPoints
-
-
-BurnDownForecast = Dict[Date, StoryPoints]
 Date = str
 
 
 class BurnDownForecaster:
-
     def __init__(self, date_skipper: DateSkipper):
         self._date_skipper = date_skipper
 
@@ -34,7 +22,9 @@ class BurnDownForecaster:
 
         while remaining_task_story_points > 0:
             if not self._date_skipper.date_should_be_skipped(next_date):
-                remaining_task_story_points = remaining_task_story_points - story_points_per_day
+                remaining_task_story_points = (
+                    remaining_task_story_points - story_points_per_day
+                )
                 if remaining_task_story_points < 0:
                     forecast[next_date] = 0
                 else:
