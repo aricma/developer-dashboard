c8c8f6f304c368f573aaf46e98890009a50504d5
aricma <adrian@aricma.com>
2024-01-12 19:46:41 +0100

    change: use VelocityTrackableTask and DeveloperVelocity in DeveloperVelocityTracker

--- business_logic/developer_velocity_tracker.py
+++ business_logic/developer_velocity_tracker.py
@@ -1,18 +1,17 @@
 from datetime import timedelta as duration
 
 from dateutil.parser import parse as to_date
-from typing import Dict, List
-
-from business_logic.models import StoryPoints
-from domain_models.task import FinishedTask, Date
+from typing import List
 
 from business_logic.models.developer_velocity import DeveloperVelocity
+from business_logic.models.velocity_trackable_task import VelocityTrackableTask
 
 
 class DeveloperVelocityTracker:
-
-    def track_team_velocity(self, tasks: List[FinishedTask]) -> Velocity:
-        team_velocity: Velocity = {}
+    def track_team_velocity(
+        self, tasks: List[VelocityTrackableTask]
+    ) -> DeveloperVelocity:
+        team_velocity: DeveloperVelocity = {}
         for task in tasks:
             self.aggregate_velocity(
                 a=team_velocity,
@@ -25,8 +24,12 @@ class DeveloperVelocityTracker:
 
         return team_velocity
 
-    def track_average_developer_velocity(self, tasks: List[FinishedTask]) -> Velocity:
-        number_of_contributing_developers = self._get_number_of_total_contributing_developers(tasks)
+    def track_average_developer_velocity(
+        self, tasks: List[VelocityTrackableTask]
+    ) -> DeveloperVelocity:
+        number_of_contributing_developers = (
+            self._get_number_of_total_contributing_developers(tasks)
+        )
         team_velocity = self.track_team_velocity(tasks)
         return {
             date: story_points / number_of_contributing_developers
@@ -34,15 +37,19 @@ class DeveloperVelocityTracker:
         }
 
     @staticmethod
-    def _get_number_of_total_contributing_developers(tasks: List[FinishedTask]) -> int:
+    def _get_number_of_total_contributing_developers(
+        tasks: List[VelocityTrackableTask],
+    ) -> int:
         contributing_developers = set()
         for task in tasks:
             for developer in task.assignees:
                 contributing_developers.add(developer)
         return len(contributing_developers)
 
-    def track_developer_velocity(self, tasks: List[FinishedTask], tracked_developer: str) -> Velocity:
-        developer_velocity: Velocity = {}
+    def track_developer_velocity(
+        self, tasks: List[VelocityTrackableTask], tracked_developer: str
+    ) -> DeveloperVelocity:
+        developer_velocity: DeveloperVelocity = {}
         for task in tasks:
             if tracked_developer in task.assignees:
                 self.aggregate_velocity(
@@ -58,13 +65,17 @@ class DeveloperVelocityTracker:
                 )
         return developer_velocity
 
-    def aggregate_velocity(self, a: Velocity, b: Velocity) -> Velocity:
+    def aggregate_velocity(
+        self, a: DeveloperVelocity, b: DeveloperVelocity
+    ) -> DeveloperVelocity:
         for date, story_points in b.items():
             self._add_day_to_velocity(velocity=a, date=date, story_points=story_points)
         return a
 
     @staticmethod
-    def filter_velocity_for_given_start_date(velocity: Velocity, start_date: str) -> Velocity:
+    def filter_velocity_for_given_start_date(
+        velocity: DeveloperVelocity, start_date: str
+    ) -> DeveloperVelocity:
         return {
             date: story_points
             for date, story_points in velocity.items()
@@ -72,7 +83,9 @@ class DeveloperVelocityTracker:
         }
 
     @staticmethod
-    def _add_day_to_velocity(velocity: Velocity, date: str, story_points: float) -> Velocity:
+    def _add_day_to_velocity(
+        velocity: DeveloperVelocity, date: str, story_points: float
+    ) -> DeveloperVelocity:
         if date in velocity.keys():
             velocity[date] += story_points
         else:
@@ -87,8 +100,8 @@ class DeveloperVelocityTracker:
 
     @staticmethod
     def _distribute_story_points_among_days_between_start_and_end_date(
-            start_date: str, end_date: str, story_points: float
-    ) -> Velocity:
+        start_date: str, end_date: str, story_points: float
+    ) -> DeveloperVelocity:
         days_between_start_and_end_date = (to_date(end_date) - to_date(start_date)).days
         if days_between_start_and_end_date == 0:
             return {start_date: story_points}
@@ -105,7 +118,7 @@ class DeveloperVelocityTracker:
                 end_date: story_points_per_day,
             }
         if days_between_start_and_end_date > 1:
-            developer_velocity: Velocity = {}
+            developer_velocity: DeveloperVelocity = {}
             for days in range(0, days_between_start_and_end_date + 1):
                 date = str(to_date(start_date) + duration(days=days))
                 developer_velocity = {**developer_velocity, date: story_points_per_day}
