a746e95070220d936138aa7b84475a78dc9c4a0a
aricma <adrian@aricma.com>
2024-01-12 14:56:12 +0100

    add: DeserializedTask

---
+++ business_logic/serializer/task.py
@@ -0,0 +1,16 @@
+from typing import List, Optional
+
+from pydantic import BaseModel
+
+TaskID = str
+DeveloperID = str
+Date = str
+
+
+class DeserializedTask(BaseModel):
+    id: TaskID
+    story_points: float
+    assignees: List[DeveloperID]
+    sub_tasks: List["DeserializedTask"]
+    date_started: Date
+    date_finished: Optional[Date] = None
