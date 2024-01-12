3cfa0ecf904e01543b50d5ce55d115eee3eeddad
aricma <adrian@aricma.com>
2024-01-12 14:55:29 +0100

    add: burn down forecastable task getter proxy

---
+++ business_logic/burn_down_forecastable_task_getter_proxy.py
@@ -0,0 +1,26 @@
+from typing import List
+
+from business_logic.interfaces.task_getter import TaskGetter
+from business_logic.models.burn_down_forecastable_task import BurnDownForecastableTask
+from business_logic.models.task import Task
+
+
+class BurnDownForecastableTaskGetterProxy(TaskGetter[BurnDownForecastableTask]):
+
+    def __init__(self, task_getter: TaskGetter[Task]):
+        self._task_getter = task_getter
+
+    def get_tasks(self) -> List[BurnDownForecastableTask]:
+        return [
+            self._to_velocity_trackable_task(task)
+            for task in self._task_getter.get_tasks()
+            if task.date_finished is not None
+        ]
+
+    @staticmethod
+    def _to_velocity_trackable_task(task: Task) -> BurnDownForecastableTask:
+        return BurnDownForecastableTask(
+            id=task.id,
+            story_points=task.story_points,
+            date_started=task.date_started,
+        )
