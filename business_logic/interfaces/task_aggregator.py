ede07b4627b21526cf720ec71234d9117a90fb9c
aricma <adrian@aricma.com>
2024-01-12 14:50:10 +0100

    add: task aggregator interface

---
+++ business_logic/interfaces/task_aggregator.py
@@ -0,0 +1,11 @@
+from abc import ABC, abstractmethod
+from typing import Generic, List, TypeVar
+
+T = TypeVar("T")
+
+
+class TaskAggregator(ABC, Generic[T]):
+
+    @abstractmethod
+    def aggregate(self, tasks: List[T]) -> T:
+        ...
