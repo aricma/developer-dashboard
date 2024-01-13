6ad6b9ce1d0b3a00747214f5764cdfd2a9faae73
aricma <adrian@aricma.com>
2024-01-13 21:24:39 +0100

    change: use make_dashboard_html in dashboard overview and developer velocity dashboard

--- web_interface/pages/make_dashboard_overview_page.py
+++ web_interface/pages/make_dashboard_overview_page.py
@@ -2,21 +2,15 @@ from typing import List, Optional
 
 from web_interface.models import Alert
 from web_interface.private.components.make_chart_html import make_chart_html
+from web_interface.private.components.make_dashboard_html import make_dashboard_html
 from web_interface.private.components.make_warning_html import make_warning_html
 from web_interface.private.features.make_dashboard_cards_html import (
     make_dashboard_cards_html,
-)
-from web_interface.private.features.make_dashboard_main_menu_item_html import (
-    make_dashboard_main_menu_item_html,
-)
-from web_interface.private.features.make_dashboard_detail_header_html import (
-    make_dashboard_overview_detail_header_html,
 )
 from web_interface.private.features.make_overview_dashboard_card_html import (
     make_overview_dashboard_card_html,
 )
 from web_interface.private.utils import (
-    make_html_template,
     read_text_file,
     join_html,
     HTMLElement,
@@ -28,64 +22,43 @@ def make_dashboard_overview_page(
     velocity_warnings: Optional[List[Alert]] = None,
     burn_down_warnings: Optional[List[Alert]] = None,
 ) -> str:
-    return make_html_template(
-        template_name="dashboard",
-        props={
-            "user_name": user_name,
-            "main_menu_children": [
-                make_dashboard_main_menu_item_html(
-                    title="Overview",
-                    link="/overview",
-                    icon_id="house-fill",
-                    is_active=True,
-                ),
-                make_dashboard_main_menu_item_html(
-                    title="Developer Velocity",
-                    link="/velocity",
-                    icon_id="speedometer",
-                ),
-                make_dashboard_main_menu_item_html(
-                    title="Task Burn Down Metric",
-                    link="/burn-down",
-                    icon_id="fire",
-                ),
-            ],
-            "detail_children": [
-                make_dashboard_overview_detail_header_html(title="Overview"),
-                make_dashboard_cards_html(
-                    children=[
-                        make_overview_dashboard_card_html(
-                            title="Current Developer Velocity",
-                            subtitle="An overview of the current developer velocity.",
-                            children=[
-                                make_chart_html(
-                                    data_file_name="./foobar-developer-velocity.json",
-                                    chart_type="velocity",
-                                ),
-                                make_warnings(warnings=velocity_warnings or []),
-                            ],
-                            description=read_text_file("velocity-overview"),
-                            link="/velocity",
-                            action_title="View Developer Velocity Details",
-                        ),
-                        make_overview_dashboard_card_html(
-                            title="Current Burn Down Overview",
-                            subtitle="An overview of the current task burn down metric.",
-                            children=[
-                                make_chart_html(
-                                    data_file_name="./foobar-task-burn-down-metric.json",
-                                    chart_type="burn-down",
-                                ),
-                                make_warnings(warnings=burn_down_warnings or []),
-                            ],
-                            description=read_text_file("burn-down-overview"),
-                            link="/burn-down",
-                            action_title="View Task Burn Down Details",
-                        ),
-                    ]
-                ),
-            ],
-        },
+    return make_dashboard_html(
+        user_name=user_name,
+        title="Overview",
+        content=[
+            make_dashboard_cards_html(
+                children=[
+                    make_overview_dashboard_card_html(
+                        title="Current Developer Velocity",
+                        subtitle="An overview of the current developer velocity.",
+                        children=[
+                            make_chart_html(
+                                data_file_name="./foobar-developer-velocity.json",
+                                chart_type="velocity",
+                            ),
+                            make_warnings(warnings=velocity_warnings or []),
+                        ],
+                        description=read_text_file("velocity-overview"),
+                        link="/velocity",
+                        action_title="View Developer Velocity Details",
+                    ),
+                    make_overview_dashboard_card_html(
+                        title="Current Burn Down Overview",
+                        subtitle="An overview of the current task burn down metric.",
+                        children=[
+                            make_chart_html(
+                                data_file_name="./foobar-task-burn-down-metric.json",
+                                chart_type="burn-down",
+                            ),
+                            make_warnings(warnings=burn_down_warnings or []),
+                        ],
+                        description=read_text_file("burn-down-overview"),
+                        link="/burn-down",
+                        action_title="View Task Burn Down Details",
+                    ),
+                ]
+            ),
+        ],
     )
 
 
--- web_interface/pages/make_dashboard_velocity_page.py
+++ web_interface/pages/make_dashboard_velocity_page.py
@@ -1,20 +1,15 @@
 from web_interface.private.components.make_chart_html import make_chart_html
+from web_interface.private.components.make_dashboard_html import make_dashboard_html
 from web_interface.private.components.make_tabs_html import (
     make_tabs_content_html,
     make_tab_panel_html,
     make_tabs_html,
     make_tab_html,
 )
-from web_interface.private.features.make_dashboard_detail_header_html import (
-    make_dashboard_overview_detail_header_html,
-)
-from web_interface.private.features.make_dashboard_main_menu_item_html import (
-    make_dashboard_main_menu_item_html,
-)
 from web_interface.private.features.make_dashboard_velocity_detail_body_html import (
     make_dashboard_velocity_detail_body_html,
 )
-from web_interface.private.utils import make_html_template, read_text_file
+from web_interface.private.utils import read_text_file
 
 
 def make_dashboard_velocity_page(
@@ -23,82 +18,61 @@ def make_dashboard_velocity_page(
     last_four_weeks_velocity_chart_data_file_name: str,
     last_eight_weeks_velocity_chart_data_file_name: str,
 ) -> str:
-    return make_html_template(
-        template_name="dashboard",
-        props={
-            "user_name": user_name,
-            "main_menu_children": [
-                make_dashboard_main_menu_item_html(
-                    title="Overview",
-                    link="/overview",
-                    icon_id="house-fill",
-                ),
-                make_dashboard_main_menu_item_html(
-                    title="Developer Velocity",
-                    link="/velocity",
-                    icon_id="speedometer",
-                    is_active=True,
-                ),
-                make_dashboard_main_menu_item_html(
-                    title="Task Burn Down Metric",
-                    link="/burn-down",
-                    icon_id="fire",
-                ),
-            ],
-            "detail_children": [
-                make_dashboard_overview_detail_header_html(title="Developer Velocity"),
-                make_tabs_html(
-                    children=[
-                        make_tab_html(
-                            panel_id="last-two-weeks",
-                            title="Last 2 Weeks",
-                            initially_selected=True,
-                        ),
-                        make_tab_html(
-                            panel_id="last-four-weeks",
-                            title="Last 4 Weeks",
-                        ),
-                        make_tab_html(
-                            panel_id="last-eight-weeks",
-                            title="Last 8 Weeks",
-                        ),
-                    ]
-                ),
-                make_tabs_content_html(
-                    children=[
-                        make_tab_panel_html(
-                            panel_id="last-two-weeks",
-                            children=[
-                                make_chart_html(
-                                    data_file_name=last_two_weeks_velocity_chart_data_file_name,
-                                    chart_type="velocity",
-                                ),
-                            ],
-                            initially_active=True,
-                        ),
-                        make_tab_panel_html(
-                            panel_id="last-four-weeks",
-                            children=[
-                                make_chart_html(
-                                    data_file_name=last_four_weeks_velocity_chart_data_file_name,
-                                    chart_type="velocity",
-                                ),
-                            ],
-                        ),
-                        make_tab_panel_html(
-                            panel_id="last-eight-weeks",
-                            children=[
-                                make_chart_html(
-                                    data_file_name=last_eight_weeks_velocity_chart_data_file_name,
-                                    chart_type="velocity",
-                                ),
-                            ],
-                        ),
-                    ]
-                ),
-                make_dashboard_velocity_detail_body_html(
-                    description=read_text_file("velocity-detail-description"),
-                ),
-            ],
-        },
+    return make_dashboard_html(
+        user_name=user_name,
+        title="Developer Velocity",
+        content=[
+            make_tabs_html(
+                children=[
+                    make_tab_html(
+                        panel_id="last-two-weeks",
+                        title="Last 2 Weeks",
+                        initially_selected=True,
+                    ),
+                    make_tab_html(
+                        panel_id="last-four-weeks",
+                        title="Last 4 Weeks",
+                    ),
+                    make_tab_html(
+                        panel_id="last-eight-weeks",
+                        title="Last 8 Weeks",
+                    ),
+                ]
+            ),
+            make_tabs_content_html(
+                children=[
+                    make_tab_panel_html(
+                        panel_id="last-two-weeks",
+                        children=[
+                            make_chart_html(
+                                data_file_name=last_two_weeks_velocity_chart_data_file_name,
+                                chart_type="velocity",
+                            ),
+                        ],
+                        initially_active=True,
+                    ),
+                    make_tab_panel_html(
+                        panel_id="last-four-weeks",
+                        children=[
+                            make_chart_html(
+                                data_file_name=last_four_weeks_velocity_chart_data_file_name,
+                                chart_type="velocity",
+                            ),
+                        ],
+                    ),
+                    make_tab_panel_html(
+                        panel_id="last-eight-weeks",
+                        children=[
+                            make_chart_html(
+                                data_file_name=last_eight_weeks_velocity_chart_data_file_name,
+                                chart_type="velocity",
+                            ),
+                        ],
+                    ),
+                ]
+            ),
+            make_dashboard_velocity_detail_body_html(
+                description=read_text_file("velocity-detail-description"),
+            ),
+        ],
     )
