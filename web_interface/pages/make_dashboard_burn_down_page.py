eb61b9c38991b4c299f2a7936f1d4beb07f918f0
aricma <adrian@aricma.com>
2024-01-13 21:26:32 +0100

    add: sub tasks section to the burn down dashboard page

--- web_interface/pages/make_dashboard_burn_down_page.py
+++ web_interface/pages/make_dashboard_burn_down_page.py
@@ -1,53 +1,32 @@
+from typing import List
+
 from web_interface.private.components.make_chart_html import make_chart_html
+from web_interface.private.components.make_dashboard_html import make_dashboard_html
 from web_interface.private.features.make_dashboard_burn_down_detail_body_html import (
     make_dashboard_burn_down_detail_body_html,
+    BurnDownPageTask,
 )
-from web_interface.private.features.make_dashboard_detail_header_html import (
-    make_dashboard_overview_detail_header_html,
+from web_interface.private.utils import (
+    read_text_file,
 )
-from web_interface.private.features.make_dashboard_main_menu_item_html import (
-    make_dashboard_main_menu_item_html,
-)
-from web_interface.private.utils import make_html_template, read_text_file
 
 
 def make_dashboard_burn_down_page(
     user_name: str,
     data_file_name: str,
+    burn_down_tasks: List[BurnDownPageTask],
 ) -> str:
-    return make_html_template(
-        template_name="dashboard",
-        props={
-            "user_name": user_name,
-            "main_menu_children": [
-                make_dashboard_main_menu_item_html(
-                    title="Overview",
-                    link="/overview",
-                    icon_id="house-fill",
-                ),
-                make_dashboard_main_menu_item_html(
-                    title="Developer Velocity",
-                    link="/velocity",
-                    icon_id="speedometer",
-                ),
-                make_dashboard_main_menu_item_html(
-                    title="Task Burn Down Metric",
-                    link="/burn-down",
-                    icon_id="fire",
-                    is_active=True,
-                ),
-            ],
-            "detail_children": [
-                make_dashboard_overview_detail_header_html(
-                    title="Task Burn Down Metric"
-                ),
-                make_chart_html(
-                    data_file_name=data_file_name,
-                    chart_type="burn-down",
-                ),
-                make_dashboard_burn_down_detail_body_html(
-                    description=read_text_file("burn-down-overview"),
-                ),
-            ],
-        },
+    return make_dashboard_html(
+        user_name=user_name,
+        title="Task Burn Down Estimation",
+        content=[
+            make_chart_html(
+                data_file_name=data_file_name,
+                chart_type="burn-down",
+            ),
+            make_dashboard_burn_down_detail_body_html(
+                description=read_text_file("burn-down-overview"),
+                subtasks=burn_down_tasks,
+            ),
+        ],
     )
--- web_interface/private/features/make_dashboard_burn_down_detail_body_html.py
+++ web_interface/private/features/make_dashboard_burn_down_detail_body_html.py
@@ -1,10 +1,60 @@
-from web_interface.private.utils import make_html_template
+import dataclasses
+from typing import List
+
+from web_interface.private.components.make_accordion_html import make_accordion_html
+from web_interface.private.components.make_accordion_item_html import (
+    make_accordion_item_html,
+)
+from web_interface.private.components.make_burn_down_task_accordion_item_html import (
+    make_burn_down_task_accordion_item_html,
+)
+from web_interface.private.utils import make_html_template, make_uuid
+
+
+@dataclasses.dataclass
+class BurnDownPageTask:
+    name: str
+    description: str
+    assignees: List[str]
+    story_points: float
+    chart_data_file_name: str
+    link_to_task_detail_page: str
+    link_to_original_task_page: str
+    estimated_finish_date: str
 
 
 def make_dashboard_burn_down_detail_body_html(
-    description: str,
+    description: str, subtasks: List[BurnDownPageTask]
 ) -> str:
+    accordion_id = "accordion-id-" + make_uuid()
     return make_html_template(
         template_name="dashboard-burn-down-detail-body",
-        props={"description": description},
+        props={
+            "description": description,
+            "subtasks_html": [
+                make_accordion_html(
+                    accordion_id=accordion_id,
+                    accordion_items=[
+                        make_accordion_item_html(
+                            accordion_id=accordion_id,
+                            accordion_item_id="accordion_item_id-" + make_uuid(),
+                            accordion_item_content_id="accordion_item_content_id-"
+                            + make_uuid(),
+                            accordion_title=task.name,
+                            accordion_content=[
+                                make_burn_down_task_accordion_item_html(
+                                    description=task.description,
+                                    link_to_task_detail_page=task.link_to_task_detail_page,
+                                    chart_data_file_name=task.chart_data_file_name,
+                                    assigned_developers=task.assignees,
+                                    estimated_finish_date=task.estimated_finish_date,
+                                    link_to_original_task=task.link_to_original_task_page,
+                                )
+                            ],
+                        )
+                        for task in subtasks
+                    ],
+                ),
+            ],
+        },
     )
