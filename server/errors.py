44e904aa482e8797a8c768e4d488e59e400cf73f
aricma <adrian@aricma.com>
2024-01-15 23:59:59 +0100

    refactor: add missing type annotations in errors

--- server/errors.py
+++ server/errors.py
@@ -2,10 +2,10 @@ from http.client import HTTPException
 
 
 class MissingAuthenticationTokenCookie(HTTPException):
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__("Missing Authentication Cookie")
 
 
 class MissingAccountForAuthenticationToken(HTTPException):
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__("Missing account for given authentication token")
