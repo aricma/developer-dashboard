27c7f1fea743479ea22e57c853cb7ef351ee324a
aricma <adrian@aricma.com>
2024-01-15 23:57:53 +0100

    remove: unused utils

--- business_logic/utils.py
+++ business_logic/utils.py
@@ -1,7 +1,6 @@
 import hashlib
-from abc import ABC, abstractmethod
 from pathlib import Path
-from typing import Generic, TypeVar, Callable, Union
+from typing import Union
 
 from yaml import load, Loader
 
@@ -24,53 +23,6 @@ def print_api_title() -> None:
 ╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═════╝  ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝  
     """
     print(server_title)
-
-
-T = TypeVar("T")
-
-
-class ReaderInterface(ABC):
-    @abstractmethod
-    def unsafe_read(self) -> str:
-        ...
-
-
-class FileReader(ReaderInterface):
-    def __init__(self, file_path: str):
-        self._file_path = file_path
-
-    def unsafe_read(self) -> str:
-        with open(self._file_path, "r") as reader:
-            return reader.read()
-
-
-class WriterInterface(ABC):
-    @abstractmethod
-    def unsafe_write(self, value: str) -> None:
-        ...
-
-
-class UpdaterInterface(ABC, Generic[T]):
-    @abstractmethod
-    def unsafe_update(self, record: T) -> None:
-        ...
-
-
-Update = Callable[[str], str]
-
-
-class FileUpdater(UpdaterInterface):
-    def __init__(
-        self, reader: ReaderInterface, writer: WriterInterface, update: Update
-    ):
-        self._reader = reader
-        self._writer = writer
-        self._update = update
-
-    def unsafe_update(self, record: T) -> None:
-        file_content = self._reader.unsafe_read()
-        updated_file_content = self._update(file_content)
-        self._writer.unsafe_write(updated_file_content)
 
 
 def hash_string_value(value: str) -> str:
