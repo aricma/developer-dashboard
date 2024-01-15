86f6a03fd908dcf0e84ce92c1f5a06625f93a3cf
aricma <adrian@aricma.com>
2024-01-15 23:58:10 +0100

    refactor: add missing type annotations in Date

--- business_logic/models/date.py
+++ business_logic/models/date.py
@@ -9,20 +9,35 @@ class Date:
         self._month = month
         self._day = day
 
-    def __gt__(self, other: "Date") -> bool:
-        return self.to_datetime_date() > other.to_datetime_date()
+    def __gt__(self, other: object) -> bool:
+        if isinstance(other, Date):
+            return self.to_datetime_date() > other.to_datetime_date()
+        else:
+            return False
 
-    def __lt__(self, other: "Date") -> bool:
-        return self.to_datetime_date() < other.to_datetime_date()
+    def __lt__(self, other: object) -> bool:
+        if isinstance(other, Date):
+            return self.to_datetime_date() < other.to_datetime_date()
+        else:
+            return False
 
-    def __eq__(self, other):
-        return self.to_datetime_date() == other.to_datetime_date()
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, Date):
+            return self.to_datetime_date() == other.to_datetime_date()
+        else:
+            return False
 
-    def __le__(self, other):
-        return self.to_datetime_date() <= other.to_datetime_date()
+    def __le__(self, other: object) -> bool:
+        if isinstance(other, Date):
+            return self.to_datetime_date() <= other.to_datetime_date()
+        else:
+            return False
 
-    def __ge__(self, other):
-        return self.to_datetime_date() >= other.to_datetime_date()
+    def __ge__(self, other: object) -> bool:
+        if isinstance(other, Date):
+            return self.to_datetime_date() >= other.to_datetime_date()
+        else:
+            return False
 
     @staticmethod
     def today() -> "Date":
